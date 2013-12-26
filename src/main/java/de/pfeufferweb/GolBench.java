6dfc8d9f300addd6cda1c5ef842a73933efd7fa3
René Pfeuffer <rene@pfeuffer-web.de>
2013-12-26 15:55:45 +0100

    gol gets instantiated

--- src/main/java/de/pfeufferweb/GolBench.java
+++ src/main/java/de/pfeufferweb/GolBench.java
@@ -1,7 +1,15 @@
 package de.pfeufferweb;
 
+
 public class GolBench {
     public GolBench(String golClassName) {
+        try {
+            Class golClassUnderTest = Class.forName(golClassName);
+            Object golUnderTest = golClassUnderTest.getConstructor()
+                    .newInstance();
+        } catch (Exception e) {
+            throw new RuntimeException("could not instantiate game of life", e);
+        }
     }
 
     public static void main(String[] args) {
