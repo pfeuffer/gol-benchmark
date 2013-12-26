ccd9761434baa45723fbc402b60c4ac94f24bedd
René Pfeuffer <rene@pfeuffer-web.de>
2013-12-26 15:32:11 +0100

    initial after mvn maven-archetype-quickstart

---
+++ src/main/java/de/pfeufferweb/App.java
@@ -0,0 +1,13 @@
+package de.pfeufferweb;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
