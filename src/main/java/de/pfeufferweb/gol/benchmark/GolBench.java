67cb5b237b728accd12bedc280af0a8bfc7a33ba
René Pfeuffer <rene@pfeuffer-web.de>
2013-12-29 21:43:18 +0100

    refactored main class

--- src/main/java/de/pfeufferweb/gol/benchmark/GolBench.java
+++ src/main/java/de/pfeufferweb/gol/benchmark/GolBench.java
@@ -1,18 +1,24 @@
 package de.pfeufferweb.gol.benchmark;
 
 public class GolBench {
+    private final Gol golUnderTest;
+
     public GolBench(String golClassName) {
         try {
             @SuppressWarnings("unchecked")
             Class<Gol> golClassUnderTest = (Class<Gol>) Class
                     .forName(golClassName);
-            Gol golUnderTest = golClassUnderTest.getConstructor().newInstance();
-            new GolBenchRunner(new GolGliderBench(golUnderTest)).run();
+            golUnderTest = golClassUnderTest.getConstructor().newInstance();
+            run();
         } catch (ClassCastException e) {
             throw new IllegalGolInterfaceException();
         } catch (Exception e) {
             throw new IllegalGolException(e);
         }
+    }
+
+    public void run() {
+        new GolBenchRunner(new GolGliderBench(golUnderTest)).run();
     }
 
     public static void main(String[] args) {
