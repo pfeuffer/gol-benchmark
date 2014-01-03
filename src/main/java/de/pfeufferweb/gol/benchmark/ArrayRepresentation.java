78d944d9d0e6a5d98bc91e9044fe5133f4f7088c
René Pfeuffer <rene@pfeuffer-web.de>
2014-01-03 15:43:27 +0100

    finished gol representation

--- src/main/java/de/pfeufferweb/gol/benchmark/ArrayRepresentation.java
+++ src/main/java/de/pfeufferweb/gol/benchmark/ArrayRepresentation.java
@@ -1,12 +1,24 @@
 package de.pfeufferweb.gol.benchmark;
 
 public class ArrayRepresentation {
+    private final Gol gol;
 
     public ArrayRepresentation(Gol gol) {
+        this.gol = gol;
     }
 
-    public String print() {
-        return "";
+    public String print(int xFrom, int xTo, int yFrom, int yTo) {
+        StringBuilder builder = new StringBuilder();
+        for (int y = yFrom; y < yTo; ++y) {
+            for (int x = xFrom; x < xTo; ++x) {
+                builder.append(gol.isAlive(x, y) ? "X" : "O");
+            }
+            builder.append("\n");
+        }
+        if (builder.length() > 0) {
+            builder.deleteCharAt(builder.length() - 1);
+        }
+        return builder.toString();
     }
 
 }
--- src/test/java/de/pfeufferweb/gol/benchmark/ArrayRepresentationTest.java
+++ src/test/java/de/pfeufferweb/gol/benchmark/ArrayRepresentationTest.java
@@ -14,12 +14,37 @@ public class ArrayRepresentationTest {
 
         @Override
         public boolean isAlive(int x, int y) {
-            return false;
+            return x == 3;
         }
     };
 
     @Test
     public void createsEmptyStringForEmptyArray() {
-        assertEquals("", new ArrayRepresentation(new GolMock()).print());
+        assertEquals("",
+                new ArrayRepresentation(new GolMock()).print(0, 0, 0, 0));
+    }
+
+    @Test
+    public void createsOForEmptyGolArrayOf1by1() {
+        assertEquals("O",
+                new ArrayRepresentation(new GolMock()).print(0, 1, 0, 1));
+    }
+
+    @Test
+    public void createsOsForEmptyGolArrayOf3by1() {
+        assertEquals("OOO",
+                new ArrayRepresentation(new GolMock()).print(0, 3, 0, 1));
+    }
+
+    @Test
+    public void createsMultipleLinesOfOsForEmptyGolArrayOf3by3() {
+        assertEquals("OOO\nOOO\nOOO",
+                new ArrayRepresentation(new GolMock()).print(0, 3, 0, 3));
+    }
+
+    @Test
+    public void createsXForAliveCellsInGol() {
+        assertEquals("OOOX\nOOOX\nOOOX",
+                new ArrayRepresentation(new GolMock()).print(0, 4, 0, 3));
     }
 }
